b1dc029050399a231313f51a7c377f5f90216021
prakashjayakumar2004 <162820285+prakashjayakumar2004@users.noreply.github.com>
2025-08-28 21:19:36 +0530

    Create BinaryPattern.java

---
+++ BinaryPattern.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc =new Scanner(System.in);
+    long a,r,c;
+    a= sc.nextLong();
+    for(r=1; r<=a; r++){
+        for(c=1;c<=a;c++){
+            System.out.print(1-((r+c)%2));
+        }
+        System.out.println();
+    }
+    }
+}
