b611c1556d1687651a300d702b2ff376e0a955e2
prakashjayakumar2004 <162820285+prakashjayakumar2004@users.noreply.github.com>
2025-08-28 20:51:36 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        float a = s.nextFloat();
+        float b = s.nextFloat();
+        float c = a*b;
+        System.out.printf("%.2f",c);
+    
+    }
+}
