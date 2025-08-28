1893080b3bb7e2ed5fe8e43220e8b8c1408e04c2
prakashjayakumar2004 <162820285+prakashjayakumar2004@users.noreply.github.com>
2025-08-28 21:18:24 +0530

    Create RhombusPattern.java

---
+++ RhombusPattern.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc =new Scanner(System.in);
+    long a,r,sp,st;
+    a= sc.nextLong();
+    for(r=1; r<=a; r++){
+        for(sp=1;sp<=a-r;sp++){
+            System.out.print(" ");
+        }
+        for(st=1;st<=a;st++)
+            System.out.print("*");
+        System.out.println();
+    }
+    }
+}
