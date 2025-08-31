1150a4a291af62effa128f1d8f396060574406a7
prakashjayakumar2004 <162820285+prakashjayakumar2004@users.noreply.github.com>
2025-08-31 20:11:44 +0530

    Create PatternNumberPyramidArrow.java

---
+++ PatternNumberPyramidArrow.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class PatternNumberPyramidArrow
+{
+public static void main(String args[])
+{
+int i, j, n;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the values ");
+n = sc.nextInt();
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= i; j++)
+System.out.print(" " + j);
+System.out.print("\n");
+}
+for (i = n - 1; i >= 1; i--)
+{
+for (j = 1; j <= i; j++)
+System.out.print(" " + j);
+System.out.print("\n");
+}
+}
+}
