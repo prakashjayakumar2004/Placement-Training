d4a79a5cc0f53cef4c2b417f332858cc9ad6b595
prakashjayakumar2004 <162820285+prakashjayakumar2004@users.noreply.github.com>
2025-08-31 20:09:28 +0530

    Create FloydsTriangle.java

---
+++ FloydsTriangle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class FloydsTriangle
+{
+ public static void main(String args[])
+ {
+
+ Scanner scan = new Scanner(System.in);
+ System.out.println("Enter the number of rows\n");
+
+ int rows = scan.nextInt();
+ System.out.println("Floyd's Triangle Generated\n");
+ int count = 1;
+ for ( int i = 1 ; i <= rows ; i++ )
+ {
+ for ( int j = 1 ; j <= i ; j++ )
+ {
+ System.out.print(count+" ");
+
+ count++;
+ }
+
+ System.out.println();
+ }
+ }
+}
