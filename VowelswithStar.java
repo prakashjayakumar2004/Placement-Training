35a5af13f6728ac2e8715a5bd75ebb2cf5ac09d8
prakashjayakumar2004 <162820285+prakashjayakumar2004@users.noreply.github.com>
2025-08-31 20:32:21 +0530

    Create VowelswithStar.java

---
+++ VowelswithStar.java
@@ -0,0 +1,11 @@
+class VowelswithStar
+{
+
+public static void main(String[] args)
+{
+String string = "Welcome to Candid Java Programming"; 
+System.out.println("Input String : "+string); 
+string = string.replaceAll("[AaEeIiOoUu]", "*"); 
+System.out.println(string); 
+}
+}
